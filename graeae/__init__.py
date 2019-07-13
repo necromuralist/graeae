126a466d5f7e7fec5ce74adbcb7d0d6b67e43e6d
necromuralist <3393973+necromuralist@users.noreply.github.com>
2019-07-12 22:34:21 -0700

    added zip-file downloader

--- graeae/__init__.py
+++ graeae/__init__.py
@@ -1,6 +1,6 @@
 from .infrastructure.base import SysLogBase
-from .infrastructure.downloader import TextDownloader
-from .infrastructure.environment import EnvironmentLoader
+from .infrastructure.downloader import TextDownloader, ZipDownloader
+from .infrastructure.environment import EnvironmentLoader, SubPathLoader
 from .timers.timer import Timer
 from .tables.tables import CountPercentage
 from .visualization.embed import EmbedHoloviews
--- graeae/infrastructure/downloader.py
+++ graeae/infrastructure/downloader.py
@@ -1,5 +1,7 @@
 # python
 from pathlib import Path
+import tempfile
+import zipfile
 
 # pypi
 import requests
@@ -53,3 +55,43 @@ class TextDownloader(SysLogBase):
                 with self.target.open("w") as writer:
                     writer.write(self._download)
         return self._download
+
+
+class ZipDownloader(SysLogBase):
+    """Downloads a zip file and unpacks it
+
+    Args:
+     url: URL to the zip file
+     target: directory to unzip the contents into
+     verbose: whether to emit statements
+    """
+    def __init__(self, url: str, target: str, verbose: bool=True):
+        super().__init__()
+        self.url = url
+        self.target = Path(target).expanduser()
+        self.verbose = verbose
+        return
+    
+    def download(self) -> None:
+        if not self.target.is_dir():
+            if self.verbose:
+                self.timer.message = "Finished Downloading and unzipping"
+                self.timer.start()
+                self.log.info("Downloading the zip file")
+            response = requests.get(self.url)
+            with tempfile.NamedTemporaryFile() as zip_file:
+                zip_file.write(response.content)
+                with zipfile.ZipFile(zip_file) as unzipper:
+                    unzipper.extractall(self.target)
+            if self.verbose:
+                self.timer.stop()
+        else:
+            if self.verbose:
+                print("Files exist, not downloading")
+        assert self.target.is_dir()
+        return
+    
+    def __call__(self):
+        """calls the download method"""
+        self.download()
+        return
